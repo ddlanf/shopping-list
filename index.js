5afb322a17793dec7539cf7adaf94c643f8fb065
Daniel Laufenberg <danlanfen26@gmail.com>
2019-12-08 11:11:34 -1000

    final edit (Rejects empty user input)

--- index.js
+++ index.js
@@ -3,37 +3,33 @@
 2.check and uncheck items on the list by clicking the "Check" button
 3. permanently remove items from the list
 */
-/*
-$('*').on( "click", function() {
-    alert("Hello");
-  });
- */
 
- //function to enter items
- 
+ //function to enter items 
 function enterItem(){
     $('#js-shopping-list-form').submit(function(event){
         event.preventDefault();
         const userInput = $(this).find('#shopping-list-entry').val();
-        //Add new item to te list
-        //Still need to reject a stirng with not letter
+        //Add new item to the list
         const list = `<li><span class="shopping-item">${userInput}</span><div class="shopping-item-controls"><button class="shopping-item-toggle"><span class="button-label">check</span></button><button class="shopping-item-delete"><span class="button-label">delete</span></button></div></li>`; 
-        $('.shopping-list').append(list); 
+       //Add list if there is user input
+        if(userInput !== ''){ $('.shopping-list').append(list);  }
     });
 }
 
 //function to check/uncheck box
 function check(){
-    $('.shopping-item-toggle').on('click', function(event){    
-    $(event.target).closest(".shopping-item-controls").prev("span").toggleClass("shopping-item__checked");
+    $('ul').on('click', '.shopping-item-toggle',function(event){    
+    //Add/remove class 
+    $(this).closest(".shopping-item-controls").prev("span").toggleClass("shopping-item__checked");
     });
 }
 
 
 //function to remove items
 function removeItem(){
-    $('.shopping-item-delete').on('click', function(event){    
-       $(event.target).closest("li").remove();
+    $('ul').on('click', '.shopping-item-delete', function(event){  
+    //remove the list  
+       $(this).closest("li").remove();
         });
 }
 

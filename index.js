45373e9e17d74d464c6ad35f4cb16cfe2358471a
Daniel Laufenberg <danlanfen26@gmail.com>
2019-12-07 15:28:07 -1000

    javascript jQuery Connected

---
+++ index.js
@@ -0,0 +1,10 @@
+/*
+1.enter items they need to purchase by entering text and hitting "Return" or clicking the "Add item" button
+2.check and uncheck items on the list by clicking the "Check" button
+3. permanently remove items from the list
+*/
+/*checking that jQuery works
+$('*').on( "click", function() {
+    alert("Hello");
+  });
+ */

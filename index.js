519523e23c1b479affa9b6857ddb81659c302da7
Daniel Laufenberg <danlanfen26@gmail.com>
2019-12-07 19:11:45 -1000

    Adding list feature

--- index.js
+++ index.js
@@ -3,22 +3,34 @@
 2.check and uncheck items on the list by clicking the "Check" button
 3. permanently remove items from the list
 */
-/*checking that jQuery works
+/*
 $('*').on( "click", function() {
     alert("Hello");
   });
  */
 
  //function to enter items
+ 
 function enterItem(){
-
+    $('#js-shopping-list-form').submit(function(event){
+        event.preventDefault();
+        const userInput = $(this).find('#shopping-list-entry').val();
+        //Add new item to te list
+        //Still need to reject a stirng with not letter
+        const list = `<li><span class="shopping-item">${userInput}</span><div class="shopping-item-controls"><button class="shopping-item-toggle"><span class="button-label">check</span></button><button class="shopping-item-delete"><span class="button-label">delete</span></button></div></li>`; 
+        $('.shopping-list').append(list); 
+    });
 }
-//function to check/uncheck items
-function check(){
 
+/*
+function check(){
 }
 
 //function to remove items
 function removeItem(){
 
 }
+*/
+
+$(enterItem);
+
